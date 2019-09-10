fa81c6b6d6ab33ae47845eb14312da470ea29e27
Siddharth Rawat <siddharth.rawat06@gmail.com>
2019-09-10 02:34:49 -0400

    Code push test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace DIS___DemoProject
     {
         static void Main(string[] args)
         {
+            //This is a Hello World Application
+            /*
+             This is for multiple
+             line
+             comments
+              * */
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue");
+            // Git test
+            Console.ReadKey();
         }
     }
 }
